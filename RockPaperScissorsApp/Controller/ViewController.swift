c90504b56ddabb96753a59b2e454429fe2cc5f6b
Salvador <Sagarcia2713@gmail.com>
2022-01-13 17:35:06 -0600

    Build the View of the app. Add a label towards the top of the app that will show the user what the opponent chose. Another label added below to display the state of the game to the user. Three buttons are added horizontally showing the user the three options to chose from. Another button added towards the bottom to reset the game. IBOutlets were connected for each UI element to the viewController swift file. IBActions implemented from each button to the viewController.

---
+++ RockPaperScissorsApp/Controller/ViewController.swift
@@ -0,0 +1,38 @@
+//
+//  ViewController.swift
+//  RockPaperScissorsApp
+//
+//  Created by Salvador Garcia on 1/13/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    @IBOutlet var opponentSignLabel: UILabel!
+    @IBOutlet var statusOfGameLabel: UILabel!
+    @IBOutlet var rockButton: UIButton!
+    @IBOutlet var paperButton: UIButton!
+    @IBOutlet var scissorsButton: UIButton!
+    @IBOutlet var playAgainButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func rockButtonPressed(_ sender: UIButton) {
+    }
+    
+    @IBAction func paperButtonPressed(_ sender: UIButton) {
+    }
+    
+    @IBAction func scissorsButtonPressed(_ sender: UIButton) {
+    }
+    
+    
+    @IBAction func playAgainButtonPressed(_ sender: UIButton) {
+    }
+}
+
