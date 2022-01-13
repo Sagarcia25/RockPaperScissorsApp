58db4a334085a771212f23fb2bb7933eb8c8d10f
Salvador <Sagarcia2713@gmail.com>
2022-01-13 16:51:33 -0600

    Build the Model object. Create a Sign swift file that contains an Enum with cases: rock, paper, scissors. Add a computed property named emoji that represents the hand gesture for each sign. Add a function named compareSign that takes in a parameter of type Sign to compare the User’s and opponent’s Sign. The function will output the state of the game. The state of the game is created through a new swift file that is also another enum named GameState. GameState contains .win, .lose. draw, .start. The opponent’s sign will be created by the randomSign function that has been implemented. The randomSign() generates a random int from 0…2. The number then determines which sign the opponent will throw. Ei 0 for Rock; 1 for paper; 2 for Scissors.

---
+++ RockPaperScissorsApp/GameState.swift
@@ -0,0 +1,12 @@
+//
+//  GameState.swift
+//  RockPaperScissorsApp
+//
+//  Created by Salvador Garcia on 1/13/22.
+//
+
+import Foundation
+
+enum GameState{
+    case start, win, lose, draw
+}
---
+++ RockPaperScissorsApp/Sign.swift
@@ -0,0 +1,51 @@
+//
+//  Sign.swift
+//  RockPaperScissorsApp
+//
+//  Created by Salvador Garcia on 1/13/22.
+//
+
+import Foundation
+
+func randomSign()->Sign{
+    let signNumber = Int.random(in: 0...2)
+    switch signNumber{
+    case 0:
+        return .rock
+    case 1:
+        return .paper
+    default:
+        return .scissors
+    }
+}
+
+enum Sign{
+    case rock
+    case paper
+    case scissors
+    
+    var emoji: String{
+        switch self{
+        case .rock:
+            return "👊"
+        case .paper:
+            return "✋"
+        case .scissors:
+            return "✌️"
+        }
+    }
+    
+    func compareSign(opponentSign: Sign)->GameState{
+
+        switch (self,opponentSign){
+        case (.rock, .rock),(.paper,.paper),(.scissors,.scissors):
+            return .draw
+        case (.rock,.scissors),(.scissors,.paper),(.paper, .rock):
+            return .win
+        case (.rock,.paper),(.paper,.scissors),(.scissors,.rock):
+            return .lose
+        }
+    }
+}
+
+
