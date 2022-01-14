16b86443428497f30777c988703033ff91bfa4a2
Salvador <Sagarcia2713@gmail.com>
2022-01-14 15:53:32 -0600

    Build the last component of the MVC design patter; the controller. Add a function called updateUI() that takes in the state of the game as a parameter. The function checks the state of the game and updates the UI. The updateUI function gets called in the viewDidLoad, playAgain button, and inside each of the Sign buttons. Within each Sign button, the action disables and hides the other buttons but reveals the playAgain button. The PlayAgain button resets the game back to its .start state. The controller now connects the model objects, Sign and GameState, to the storyboard elements.

--- RockPaperScissorsApp/Controller/ViewController.swift
+++ RockPaperScissorsApp/Controller/ViewController.swift
@@ -17,22 +17,78 @@ class ViewController: UIViewController {
     @IBOutlet var scissorsButton: UIButton!
     @IBOutlet var playAgainButton: UIButton!
     
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        updateUI(gameStateOfTheGame: .start)
+        
     }
 
     @IBAction func rockButtonPressed(_ sender: UIButton) {
+        let mySign = Sign.rock
+        paperButton.isHidden = true
+        scissorsButton.isHidden = true
+        playGame(mySign: mySign)
     }
     
     @IBAction func paperButtonPressed(_ sender: UIButton) {
+        let mySign = Sign.paper
+        rockButton.isHidden = true
+        scissorsButton.isHidden = true
+        playGame(mySign: mySign)
     }
     
     @IBAction func scissorsButtonPressed(_ sender: UIButton) {
+        let mySign = Sign.scissors
+        paperButton.isHidden = true
+        rockButton.isHidden = true
+        playGame(mySign: mySign)
     }
     
     
     @IBAction func playAgainButtonPressed(_ sender: UIButton) {
+        updateUI(gameStateOfTheGame: .start)
+    }
+    
+    func updateUI(gameStateOfTheGame: GameState){
+            switch gameStateOfTheGame {
+            case .start:
+                view.backgroundColor = UIColor.blue
+                opponentSignLabel.text = "🤖"
+                playAgainButton.isHidden =  true
+                rockButton.isHidden = false
+                paperButton.isHidden = false
+                scissorsButton.isHidden = false
+                rockButton.isEnabled = true
+                paperButton.isEnabled = true
+                scissorsButton.isEnabled = true
+                statusOfGameLabel.text = "Start!"
+            case .win:
+                view.backgroundColor = UIColor.green
+                statusOfGameLabel.text = "You Won!"
+                playAgainButton.isHidden = false
+            case .lose:
+                view.backgroundColor = UIColor.systemPink
+                statusOfGameLabel.text = "You Lost!"
+                playAgainButton.isHidden = false
+            case .draw:
+                view.backgroundColor = UIColor.orange
+                statusOfGameLabel.text = "It's a Tie!"
+                playAgainButton.isHidden = false
+            }
+
+    }
+    
+    func playGame(mySign: Sign){
+        rockButton.isEnabled = false
+        paperButton.isEnabled = false
+        scissorsButton.isEnabled = false
+        let opponentSign = randomSign()
+        let gameState = mySign.compareSign(opponentSign: opponentSign)
+        opponentSignLabel.text = opponentSign.emoji
+        updateUI(gameStateOfTheGame: gameState)
+
     }
 }
 
--- RockPaperScissorsApp/Model/Sign.swift
+++ RockPaperScissorsApp/Model/Sign.swift
@@ -46,6 +46,8 @@ enum Sign{
             return .lose
         }
     }
+    
+
 }
 
 
